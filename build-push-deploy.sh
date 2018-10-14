3a9bad0e9384c8b85b51136ee02b98a5bc3cc9c9
Adam Sandor <adam.sandor@live.com>
2018-10-14 16:24:49 -0400

    automatic increment of container version number based on current deployment

--- build-push-deploy.sh
+++ build-push-deploy.sh
@@ -2,10 +2,28 @@
 
 set -e
 
+APP=phone-node
+
+function getVersion() {
+    kubectl get deployment $APP -o jsonpath='{.spec.template.spec.containers[0].image}' | egrep -o ':.+' | cut -c 2-
+}
+
+if [ -z $1 ]; then
+PREV_VERSION=$(getVersion)
+
+VERSION=$(($PREV_VERSION+1))
+else
 VERSION=$1
+fi
 
-docker build -t clusteringsolutionsshowdown/phone-node:${VERSION} .
+echo "Building $APP version $VERSION"
 
-docker push clusteringsolutionsshowdown/phone-node:${VERSION}
+docker build -t clusteringsolutionsshowdown/$APP:${VERSION} .
 
-kubectl set image deployment/phone-node phone-node=clusteringsolutionsshowdown/phone-node:${VERSION}
+echo "Pushing $APP version $VERSION"
+
+docker push clusteringsolutionsshowdown/$APP:${VERSION}
+
+echo "Deploying $APP version $VERSION"
+
+kubectl set image deployment/$APP $APP=clusteringsolutionsshowdown/$APP:${VERSION}
